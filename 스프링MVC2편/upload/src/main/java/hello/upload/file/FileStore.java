da3bb701580509d081d107e3321278d73e073175
seunggeuncho <wkfyddl1tp@naver.com>
2024-05-17 14:09:07 +0900

    240517

---
+++ 스프링MVC2편/upload/src/main/java/hello/upload/file/FileStore.java
@@ -0,0 +1,60 @@
+package hello.upload.file;
+
+import hello.upload.domain.UploadFile;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@Slf4j
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String filename){
+        return fileDir + filename;
+    }
+
+    public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
+        List<UploadFile> storeFileResult = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if(!multipartFile.isEmpty()){
+                storeFileResult.add(storeFile(multipartFile));
+            }
+        }
+        return storeFileResult;
+    }
+
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+        if(multipartFile.isEmpty()){
+            return null;
+        }
+
+        String originalFilename = multipartFile.getOriginalFilename();
+        //image.png
+
+        //서버에 저장하는 파일명
+        String storeFileName = createStoreFileName(originalFilename);
+        multipartFile.transferTo(new File(getFullPath(storeFileName)));
+        return new UploadFile(originalFilename, storeFileName);
+    }
+
+    private String createStoreFileName(String originalFilename) {
+        String uuid = UUID.randomUUID().toString();
+        String ext = extracted(originalFilename);
+        return uuid + "." + ext;
+    }
+
+    private String extracted(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+}
