646f914641fd484a773af2990dea3be82ef4036f
seunggeuncho <wkfyddl1tp@naver.com>
2024-05-18 14:32:58 +0900

    240518

---
+++ 스프링MVC2편/typeconverter/src/main/java/hello/typeconverter/controller/Filter.java
@@ -0,0 +1,22 @@
+package hello.typeconverter.controller;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class Filter implements jakarta.servlet.Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        jakarta.servlet.Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+        jakarta.servlet.Filter.super.destroy();
+    }
+}
