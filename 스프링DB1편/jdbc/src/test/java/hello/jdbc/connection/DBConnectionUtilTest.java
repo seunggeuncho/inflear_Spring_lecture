6598d3d5254e31bc88101dbc4b64469e6cb0ee30
seunggeuncho <wkfyddl1tp@naver.com>
2024-05-20 15:36:20 +0900

    240520

---
+++ 스프링DB1편/jdbc/src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
@@ -0,0 +1,15 @@
+package hello.jdbc.connection;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+public class DBConnectionUtilTest {
+
+    @Test
+    void connection(){
+        Connection connection = DBConnectionUtil.getConnection();
+        Assertions.assertThat(connection).isNotNull();
+    }
+}
