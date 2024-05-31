2ebd229fd65be08c8a865d3077fe9919e974e01a
seunggeuncho <wkfyddl1tp@naver.com>
2024-05-31 12:25:39 +0900

    240531

--- 스프링DB2편/source/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ 스프링DB2편/source/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -61,5 +62,38 @@ class MemberServiceTest {
 
         Assertions.assertTrue(memberRepository.find(username).isPresent());
         Assertions.assertTrue(logRepository.find(username).isPresent());
+    }
+
+    @Test
+    void outerTxOn_fail(){
+        //given
+        String username="로그예외_outerTxOn_fail";
+
+        assertThatThrownBy(() -> memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+
+        Assertions.assertTrue(memberRepository.find(username).isEmpty());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    @Test
+    void recoverException_fail(){
+        //given
+        String username="로그예외_recoverException_fail";
+
+        assertThatThrownBy(() -> memberService.joinV2(username)).isInstanceOf(UnexpectedRollbackException.class);
+
+        Assertions.assertTrue(memberRepository.find(username).isEmpty());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    @Test
+    void recoverException_success(){
+        //given
+        String username="로그예외_recoverException_success";
+
+        memberService.joinV2(username);
+
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
     }
 }
