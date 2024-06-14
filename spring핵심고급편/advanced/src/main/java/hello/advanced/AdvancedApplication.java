04c068422d43908e242226cd8681cf979c7f760e
seunggeuncho <wkfyddl1tp@naver.com>
2024-06-14 23:49:31 +0900

    240614

--- spring핵심고급편/advanced/src/main/java/hello/advanced/AdvancedApplication.java
+++ spring핵심고급편/advanced/src/main/java/hello/advanced/AdvancedApplication.java
@@ -2,6 +2,8 @@ package hello.advanced;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
 
 @SpringBootApplication
 public class AdvancedApplication {
